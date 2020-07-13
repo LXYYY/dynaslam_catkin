c9633d40d33ca08acb329db4a76165147af14e03
Xiangyu Liu <xiangyu002@e.ntu.edu.sg>
2020-07-13 17:42:12 +0800

    add mask_results.hpp

---
+++ tf_mask_rcnn_detector/include/tf_mask_rcnn_detector/mask_results.hpp
@@ -0,0 +1,12 @@
+#ifndef TF_MASK_RCNN_DETECTOR_MASK_RESULTS_HPP
+#define TF_MASK_RCNN_DETECTOR_MASK_RESULTS_HPP
+
+namespace tf_mask_rcnn_detector
+{
+class MaskResults
+{
+
+};
+}  // namespace tf_mask_rcnn_detector
+
+#endif
